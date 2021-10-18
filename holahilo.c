e0d2dcf25f6f7015fc181f0964fe88861f3a5b2f
Maximiliano Villegas <maxivillegas115@gmail.com>
2021-10-18 23:22:35 +0000

    holahilo

--- holahilo.c
+++ holahilo.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define NUM_THREADS 5
+#define NUM_THREADS 10
+int saldo;
 
 void *printHola(void *arg){
     printf("Hola desde un hilo\n");
-    while(1){
-        sleep(1);
-    }
+    int saldolocal = saldo;
+    saldolocal += 100;
+    saldo = saldolocal;
     pthread_exit(NULL);
 }
 
 int main(){
     pthread_t threads[NUM_THREADS];
+    saldo = 0;
     for(int i = 0; i < NUM_THREADS; i++){
         pthread_create(&threads[i],NULL,printHola,NULL);
     }
-
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_join(&threads[i],NULL);
+    }
+    printf("El valor del saldo es %d", salida);
     pthread_exit(NULL);
     return 0;
 }
